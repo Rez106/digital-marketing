67a98e1082f0b10a0488c538cd921c0ec93cd3e2
Reza Darvishi <darvishireza8181@gmail.com>
2024-07-27 22:15:58 +0330

    add custom colors in tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        "brand-green": "#2DCA72",
+        "secondary-gray": "#C9C9C9",
+        "main-black": "#0D0D0D",
+      },
+    },
+  },
+  plugins: [],
+};
