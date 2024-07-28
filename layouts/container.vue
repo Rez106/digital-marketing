802226b33c6da33c9ded363beb3724315d1e2716
Reza Darvishi <darvishireza8181@gmail.com>
2024-07-28 18:41:28 +0330

    create default and container layout

---
+++ layouts/container.vue
@@ -0,0 +1,5 @@
+<template>
+  <section class="max-w-[1300px] px-2 mx-auto">
+    <slot />
+  </section>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="min-w-full min-h-dvh bg-main-black">
+    <slot />
+  </div>
+</template>
