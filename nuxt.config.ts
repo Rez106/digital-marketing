f55d2586217d395c807f9bbae9241fa63fb9ec83
Reza Darvishi <darvishireza8181@gmail.com>
2024-07-28 20:35:03 +0330

    add swiper module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-07-28",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon", "@vueuse/nuxt", "@nuxt/image"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxt/icon",
+    "@vueuse/nuxt",
+    "@nuxt/image",
+    "nuxt-swiper",
+  ],
   css: ["@/assets/css/main.css"],
   app: {
     head: {
