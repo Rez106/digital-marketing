8a298454a54a8450aeb4c588af55592f3277eac5
Reza Darvishi <darvishireza8181@gmail.com>
2024-07-28 14:29:59 +0330

    remove @nuxtjs/fonts and implement fonts with @layer in main.css file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: "2024-07-28",
   devtools: { enabled: true },
-  modules: [
-    "@nuxtjs/tailwindcss",
-    "@nuxt/icon",
-    "@vueuse/nuxt",
-    "@nuxt/image",
-    "@nuxt/fonts"
-  ],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon", "@vueuse/nuxt", "@nuxt/image"],
+  css: ["@/assets/css/main.css"],
+  app: {
+    head: {
+      link: [{ rel: "icon", href: "logo.svg", type: "image/svg+xml" }],
+    },
+  },
 });
