0deb61f3f09cd7ecb7b158b9035b9fdd6cb4e701
Reza Darvishi <darvishireza8181@gmail.com>
2024-07-30 17:20:28 +0330

    change usable breakpoint variables to useDisplay composable

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -2,22 +2,24 @@
   <header class="w-full">
     <NuxtLayout
       name="container"
-      class="flex 2xl:items-start items-center lg:justify-between pt-5"
+      class="flex 2xl:items-start items-center md:justify-between pt-5"
     >
       <TheLogo :width="100" :height="100" />
-      <NavbarDesktop v-if="$device.isDesktop" />
-      <NavbarMobile v-if="$device.isMobile" @closeMenu="toggleMenu" :isOpen />
+      <NavbarDesktop v-if="mdAndLarger" />
+      <NavbarMobile v-if="mdAndSmaller" @closeMenu="toggleMenu" :isOpen />
       <TheMenuToggle
-        v-if="$device.isMobile"
+        v-if="mdAndSmaller"
         @openMenu="toggleMenu"
-        class="order-1"
+        class="max-md:order-1"
       />
-      <TheContactUs class="max-2xl:mr-auto ml-4" />
+      <TheContactUs class="max-md:mr-auto ml-4" />
     </NuxtLayout>
   </header>
 </template>
 
 <script setup>
+  const { mdAndLarger, mdAndSmaller } = useDisplay();
+
   const isOpen = ref(false);
 
   const toggleMenu = (status) => {
