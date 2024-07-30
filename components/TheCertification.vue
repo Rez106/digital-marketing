5dde4db9ccdfb9932ce40332d1ee3db5a2e539e3
Reza Darvishi <darvishireza8181@gmail.com>
2024-07-30 17:23:03 +0330

    style certification section for responsive and add swiper for displaying in smaller screens

--- components/TheCertification.vue
+++ components/TheCertification.vue
@@ -2,19 +2,67 @@
   <section class="w-full mt-10 py-5">
     <NuxtLayout name="container" class="flex flex-col">
       <div
-        class="w-fit flex items-center justify-between gap-4 text-secondary-gray font-medium"
+        class="w-fit flex items-center justify-between gap-4 text-secondary-gray font-medium max-md:mx-auto"
       >
         <h3>برجسته در</h3>
         <div class="w-0.5 h-5 bg-secondary-gray opacity-15 rounded-lg"></div>
         <h3 class="opacity-45">گواهینامه ما</h3>
       </div>
-      <div class="flex items-center gap-10 justify-between mt-5">
-        <div
-          class="flex-1 px-3 lg:px-10 h-24 flex items-center justify-center flex-wrap bg-[#181818] rounded-lg"
-          v-for="(item, index) in companies"
-          :key="index"
+      <div class="w-full">
+        <Swiper
+          v-if="xlAndSmaller"
+          :modules="[SwiperNavigation]"
+          :space-between="10"
+          :navigation="{
+            enabled: true,
+            nextEl: '.custom-next-button',
+            prevEl: '.custom-prev-button',
+          }"
+          :breakpoints="{
+            1200: {
+              slidesPerView: 6,
+            },
+            1000: {
+              slidesPerView: 4,
+            },
+            700: {
+              slidesPerView: 3,
+            },
+            600: {
+              slidesPerView: 2,
+            },
+            400: {
+              slidesPerView: 1,
+            },
+          }"
+          class="mt-5 !px-10 !h-24 max-sm:!w-10/12 max-sm:mx-auto !w-full"
         >
-          <NuxtImg :src="item" class="w-8/12" />
+          <SwiperSlide v-for="(item, index) in companies" :key="index">
+            <div
+              class="px-3 lg:px-10 h-full flex items-center justify-center flex-wrap bg-[#181818] rounded-lg cursor-pointer"
+            >
+              <NuxtImg :src="item" class="w-8/12" />
+            </div>
+          </SwiperSlide>
+          <button
+            class="disabled:hidden custom-next-button bg-gradient-to-r from-main-black to-transparent h-full text-secondary-gray absolute top-1/2 -translate-y-1/2 left-0 z-50 p-2 cursor-pointer"
+          >
+            <Icon name="mdi:chevron-left" size="x-large" />
+          </button>
+          <button
+            class="disabled:hidden custom-prev-button text-secondary-gray bg-gradient-to-l from-main-black to-transparent h-full absolute top-1/2 -translate-y-1/2 right-0 z-50 cursor-pointerdisabled:hidden"
+          >
+            <Icon name="mdi:chevron-right" size="x-large" />
+          </button>
+        </Swiper>
+        <div v-else class="w-full flex items-center gap-5 h-24 mt-10">
+          <div
+            v-for="(item, index) in companies"
+            :key="index"
+            class="flex-1 px-3 h-full flex items-center justify-center flex-wrap bg-[#181818] rounded-lg max-xl:cursor-pointer"
+          >
+            <NuxtImg :src="item" class="w-8/12" />
+          </div>
         </div>
       </div>
     </NuxtLayout>
@@ -22,6 +70,8 @@
 </template>
 
 <script setup>
+  const { xlAndLarger, xlAndSmaller } = useDisplay();
+
   const companies = [
     "/images/featured/asus-logo.svg",
     "/images/featured/allianz-1.svg",
