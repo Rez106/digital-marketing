20165967be3a30b958ab0d6d7d49e56b09919deb
Reza Darvishi <darvishireza8181@gmail.com>
2024-07-31 19:13:57 +0330

    change header to be sticky on top and have a blur background

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,8 +1,8 @@
 <template>
-  <header class="w-full">
+  <header class="w-full sticky top-0 left-0 z-[99999] backdrop-blur-lg">
     <NuxtLayout
       name="container"
-      class="flex 2xl:items-start items-center md:justify-between pt-5"
+      class="flex 2xl:items-start items-center md:justify-between py-1 sm:py-3"
     >
       <TheLogo :width="100" :height="100" />
       <NavbarDesktop class="max-md:hidden" />
